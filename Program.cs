37ff72bc75484dc9423e428737ab20e80c4bf785
Анастасия <94762289+sysoewa@users.noreply.github.com>
2022-06-27 17:00:00 +0300

    семинар 3

--- Program.cs
+++ Program.cs
@@ -1,30 +1,74 @@
-﻿// // Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// Console.Write("Введите трёхзначное число: ");
+﻿
+// // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Console.Write("Введите 5-значное число: ");
 // int num = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine (num/10 % 10);
 
+// int count1 = Convert.ToInt32(Console.ReadLine());
+// int count2 = Convert.ToInt32(Console.ReadLine());
+// int count3 = Convert.ToInt32(Console.ReadLine());
+// int count4 = Convert.ToInt32(Console.ReadLine());
+// int count5 = Convert.ToInt32(Console.ReadLine());
 
-// // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// Console.Write("Введи число из промежутка от 1 до 1000: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// if (num < 100) {
-//     Console.Write ("третьей цифры нет");
+// count1 = num / 10000;
+// count2 = num /1000 % 10;
+// count3 = num /100 % 10;
+// count4 = num % 100;
+// count5 = num % 10;
+
+// if  (count1 == count5)  {
+//     Console.Write ("1 и 5 числа равны");
 // }
-//  if (num > 100) {
-//     Console.Write ( num % 10);
-//  }
-    
+// else {
+//     Console.Write ("1 и 5 числа не равны");
+// }
+// if  (count2 == count4) {
+//     Console.Write ("2 и 4 числа равны");
+// }
+// else {
+//     Console.Write ("2 и 4 числа не равны");
+// }
 
-// // Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-Console.Write("Введи число недели: ");
-int day = Convert.ToInt32(Console.ReadLine());
-if (day==6 || day==7) {
-    Console.WriteLine("выходной день");
+// Console.WriteLine("num");
+
+
+
+// // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// Console.Clear();
+// double distAB (int x1, int y1, int z1, int x2, int y2, int z2) {
+//     return Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2) + Math.Pow((z2 - z1),2));
+// }
+// Console.WriteLine("Введите координату x1: ");
+// int xA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату y1: ");
+// int yA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату z1: ");
+// int zA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату x2: ");
+// int xB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату y2: ");
+// int yB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату z2: ");
+// int zB = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Длина отрезка"+ distAB(xA, yA, zA, xB, yB, zB));
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.Clear();
+void listSqrt (int n) {
+
+    Console.Write (n + "-> ");
+    int count = 1;
+    while (count <= n) {
+        Console.Write(Math.Pow (count, 3)+ ", ");
+        count++;
+    }
 }
-else if (day < 1 || day > 7) {
-     Console.WriteLine("некорректный день");
-       }
-else {
-     Console.WriteLine("не выходной день");
-        }  
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+listSqrt(num);
 
