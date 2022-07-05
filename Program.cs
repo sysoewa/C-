e1162ac29d6051529f765eb269999898654d349d
Анастасия <94762289+sysoewa@users.noreply.github.com>
2022-07-05 22:10:52 +0400

    C#5

--- Program.cs
+++ Program.cs
@@ -1,48 +1,144 @@
-﻿// // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// Console.Write("Введите число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int S =0;
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
-// while (num > 0) {
-// S += num % 10;
-// num = (num - num % 10)/10;
+// int [] CreateRandomArray (int size){
+//     int [] array = new int [size];
+//     for (int i = 0; i < size; i++){
+//         array[i] = new Random().Next (100,1000);
+//     }
+//     return array;
 // }
 
-// Console.WriteLine ("Sum numbers of number is " + S );
 
-
-
-// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-
-// int [] numbers = new int[8];
-// Console.Write("[");
-
-// for (int i = 0; i < numbers.Length; i++)
-//  {
-//     numbers [i] = new Random().Next(0, 11);
-//     Console.Write(" " + Method (i) + " ");
-//  }
-// Console.Write("]");
-// int Method (int a)
+// void ShowArray(int[] array)
 // {
-//     return numbers[a];
+//     Console.WriteLine();
+//     Console.Write("Исходный массив: [");
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         Console.Write($"{array[i]}, ");
+//     }
+//     Console.Write($"{array[array.Length - 1]}]");
+//     Console.WriteLine();
 // }
 
-
-
-// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-
-// Console.Write ("Введите число А: ");
-// int A = Convert.ToInt32(Console.ReadLine());
-// Console.Write ("Введите число B: ");
-// int B = Convert.ToInt32(Console.ReadLine());
+// void Numbers (int[] array)
 // {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0) count++;
+//     }
 
-//     for (int i = 1; i <= B; i++) ;
-
+//     Console.Write($"Количество четных элементов в массиве: {count}");
 // }
-// double result = 1;
-// result = (Math.Pow (A , B));
+
+// int[] AskUser()
+// {
+//     Console.Write("Введите размер массива: ");
+//     int size = Convert.ToInt32(Console.ReadLine());
+//     int[] array = CreateRandomArray(size);
+//     ShowArray(array);
+
+//     return array;
+// }
+
+// Numbers(AskUser());
 
 
-// Console.WriteLine( $"{A} ^ {B} = {result}");
+
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// int [] CreateRandomArray (int size) 
+// {
+//     int [] array = new int [size];
+//     for (int i=0; i < size; i++) 
+//     {
+//         array [i] = new Random().Next (0, 20);
+//     } 
+//     return array;
+// }
+
+
+// void ShowArray (int [] array) 
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write (array [i] + " ");
+//     }
+// }
+
+// int [] MyArray = CreateRandomArray (6);
+// ShowArray (MyArray);
+
+
+// void Sum (int [] array) {
+//     int result = 0;
+//     for (int i = 1; i < array.Length; i = i + 2)
+//     {
+//         result = result + array [i];
+//     }
+//     Console.WriteLine("Сумма чисел, стоящих на нечётных позициях в массиве: " + result);
+// }
+
+// Sum (MyArray);
+
+
+
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int [] CreateRandomArray(int size)
+{
+    int [] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0, 20);
+    }
+    return array;
+}
+
+
+void ShowArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int [] MyArray = CreateRandomArray(6);
+ShowArray(MyArray);
+
+
+
+void DifMinMax(int [] array)
+{
+    int max = 0;
+    int min = array[0];
+    int result  = 0;
+    
+
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+        
+    }
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+    }
+    Console.WriteLine ("max number: " + max + "min number: " + min );
+    result = max - min;
+    Console.WriteLine("difference between min and max: " + result );
+}
+
+DifMinMax (MyArray);
+
+
