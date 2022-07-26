60749015f198a824850f351fd0b2123fea9af7f2
Анастасия <94762289+sysoewa@users.noreply.github.com>
2022-07-26 13:23:43 +0400

    Add files via upload

---
+++ семинар 4/Program.cs
@@ -0,0 +1,48 @@
+﻿// // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int S =0;
+
+// while (num > 0) {
+// S += num % 10;
+// num = (num - num % 10)/10;
+// }
+
+// Console.WriteLine ("Sum numbers of number is " + S );
+
+
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// int [] numbers = new int[8];
+// Console.Write("[");
+
+// for (int i = 0; i < numbers.Length; i++)
+//  {
+//     numbers [i] = new Random().Next(0, 11);
+//     Console.Write(" " + Method (i) + " ");
+//  }
+// Console.Write("]");
+// int Method (int a)
+// {
+//     return numbers[a];
+// }
+
+
+
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Console.Write ("Введите число А: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.Write ("Введите число B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// {
+
+//     for (int i = 1; i <= B; i++) ;
+
+// }
+// double result = 1;
+// result = (Math.Pow (A , B));
+
+
+// Console.WriteLine( $"{A} ^ {B} = {result}");
